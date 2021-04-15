6699eaa56589c15bea016cd69da9577651689bb5
unknown <frankobidike20@gmail.com>
2021-04-15 17:42:47 +0100

    isLoggedin

--- src/test.js
+++ src/test.js
@@ -4,23 +4,26 @@ class Test extends React.Component {
     constructor() {
         super();
         this.state = { 
-            data:{
                 isLoggedin:true,
             loginValue: 'You are Logged in',
             logoutValue:'You are logged out'
-            }
          }
          this.loggedIn=this.loggedIn.bind(this)
     }
     loggedIn=()=>{
         this.setState(prevState=>{
-            const login= prevState.data.map(function(item){
-                if(item.isLoggedin){
-                    item.isLoggedin= !item.isLoggedin
-                }return item
-            })
-            return {
-                data:login
+            // const login= prevState.data.map(function(item){
+            //     if(item.isLoggedin){
+            //         item.isLoggedin= !item.isLoggedin
+            //     }return item
+            // })
+            // return {
+            //     data:login
+            // }
+
+            return{
+                ...prevState,
+                isLoggedin: !prevState.isLoggedin
             }
         })
     }
@@ -28,7 +31,7 @@ class Test extends React.Component {
     render() { 
         return ( 
             <div>
-                <h1>{this.state.data.isLoggedin ?this.state.data.loginValue:this.state.data.logoutValue}</h1>
+                <h1>{this.state.isLoggedin ?this.state.loginValue:this.state.logoutValue}</h1>
                <button onClick={this.loggedIn}>{this.state.isLoggedin?'Log out':'Log in'}</button>
             </div>
          );
